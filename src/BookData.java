e3393f8d6d6debab6cab51dd58edf37248a77dc8
kcheung3102 <kcheung3102@gmail.com>
2019-10-30 16:38:44 -0400

    commit

---
+++ src/BookData.java
@@ -0,0 +1,45 @@
+public class BookData {
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private String bookTitle;
+    private String author;
+    private String description;
+    private double price;
+
+
+    BookData() {
+
+
+    }
+
+}
